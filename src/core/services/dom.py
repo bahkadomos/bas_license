f933253807b04b6c734e4c05bf75c567ca716ab6
bahkadomos <bahladamos@protonmail.com>
2024-11-18 21:25:30 +0300

    fix: Add type annotation

--- src/core/services/dom.py
+++ src/core/services/dom.py
@@ -6,7 +6,7 @@ class HTMLParser:
     def __init__(self):
         self._parser = etree.HTMLParser()
 
-    def get_by_xpath(self, html: str, query: str):
+    def get_by_xpath(self, html: str, query: str) -> str | None:
         dom = etree.fromstring(html, self._parser)
         data = dom.xpath(query)
         if isinstance(data, Sequence) and isinstance(data[0], str):
